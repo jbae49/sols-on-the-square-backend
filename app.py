82087ede5bddad2b84b309f1173a0f003d52b72c
jbae49@wisc.edu <jbae49@wisc.edu>
2024-02-16 23:13:06 -0600

    added track-visit method in flask, fingerprinting doesn't work :\

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 import mysql.connector
 import json
@@ -8,14 +8,20 @@ import uuid
 
 load_dotenv(".env")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DATABASE_NAME = os.getenv("MYSQL_DATABASE_NAME")
+
 app = Flask(__name__)
 CORS(app)
+# app.config["CORS_HEADERS"] = "Content-Type"
 
 db_config = {
-    "user": "root",
+    "user": MYSQL_USER,
     "password": MYSQL_PASSWORD,
-    "host": "localhost",
-    "database": "sols",
+    "host": MYSQL_HOST,
+    "database": MYSQL_DATABASE_NAME,
+    "autocommit": True,
 }
 
 db_connection = mysql.connector.connect(**db_config)
@@ -30,24 +36,28 @@ def index():
 @app.route("/track-visit", methods=["GET"])
 def track_visit():
     visitor_id = request.cookies.get("visitor_ud")
+    ip_address = request.remote_addr
 
-    db_cursor.execute("SELECT CookieID FROM Users WHERE CookieID = %s", (user_ip,))
+    if not visitor_id:
+        visitor_id = uuid.uuid4().hex
+        response = make_response(jsonify({"message": "New visitor tracked"}))
+        response.set_cookie(
+            "visitor_id", visitor_id, max_age=60 * 60 * 24 * 365
+        )  # Expires in 1 year
+    else:
+        response = jsonify({"message": "Returning visitor tracked"})
+
+    db_cursor.execute("SELECT CookieID FROM Users WHERE CookieID = %s", (visitor_id,))
     result = db_cursor.fetchone()
 
     if not result:
-        db_cursor.execute("INSERT INTO Users (CookieID) VALUES (%s)", (user_ip,))
+        db_cursor.execute(
+            "INSERT INTO Users (CookieID, IPAddress) VALUES (%s,%s)",
+            (visitor_id, ip_address),
+        )
         db_connection.commit()
 
-    return jsonify({"message": "Visit recorded"}), 200
-
-
-# @app.route('/select-language', methods=['POST'])
-# def select_language():
-#     data = request.get_json()
-#     language = data.get('language')
-
-#     user_ip = request.remote_addr
-#     print('User IP:', user_ip)
+    return response
 
 
 if __name__ == "__main__":
